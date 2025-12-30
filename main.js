bbd61d65da4e244f23aa602196fc86407af553ce
gopin8468@gmail.com <gopin8468@gmail.com>
2025-12-30 12:30:30 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ app.get('/',(req,res)=>{
   res.json({msg:"hello students"})
 })
 
+//CRUD functionality of movies
+//R - for reading
+app.get('/movies',()=>{
+
+})
+
+//c - for creating
+
+app.post('/movies',()=>{
+  
+})
+
+//u-for update
+app.put('/movies/:id',()=>{
+  
+})
+
+//D - for delete
+app.delete('/movies/:id',()=>{
+  
+})
+
 app.listen(PORT,()=>{
   console.log(`server run port http://localhost:${PORT}`);
   
