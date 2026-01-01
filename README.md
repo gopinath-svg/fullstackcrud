68d5ca260d0ac6c02e21e0cbd2486c3330c38c7a
gopin8468@gmail.com <gopin8468@gmail.com>
2026-01-01 20:23:17 +0530

    mongodb connected

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 4.git add .
 5.git commit -m "your message"
 6.git push -u origin main 
+
+
+# db options
+mongodb
+
+#process means temial 
+
+#npm i dotenv
+
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import moviesRoute from "./routes/movies.route.js";
+import connectDB from './lib/db.js';
 const app = express();
 const PORT = 3000;
 
+
+//connect db
+connectDB();
 
 app.get('/',(req,res)=>{
   res.json({msg:"hello students"})
